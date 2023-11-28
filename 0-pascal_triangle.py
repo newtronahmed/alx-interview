0d85e5a90bc92c3d0446c33de7d4fa1de74cae90
ahmed <bazubairu@st.ug.edu.gh>
2023-11-28 15:18:57 +0000

    why

--- 0-pascal_triangle.py
+++ 0-pascal_triangle.py
@@ -21,13 +21,3 @@ def pascal_triangle(n):
         triangle.append(line)
     return triangle
 
-def print_triangle(triangle):
-    """
-    Print the triangle
-    """
-    for row in triangle:
-        print("[{}]".format(",".join([str(x) for x in row])))
-
-
-if __name__ == "__main__":
-    print_triangle(pascal_triangle(5))
