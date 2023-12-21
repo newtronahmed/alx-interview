588930360d751c45f79fa14e40f0f71d83ec5ee0
neutronahmed <bazubairu@st.ug.edu.gh>
2023-12-21 23:36:58 +0000

    implementaion using counter

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,36 +5,35 @@
 import sys
 from collections import Counter
 
-def print_status(status_codes, size):
+def print_status(status_counts, size):
     """Prints status code and size"""
     print("File size: {}".format(size))
-    for i in sorted(status_codes.keys())
-        if status_codes[i] != 0:
-            print("{}: {:d}".format(i, status_codes[i]))
+    for status_code, count in sorted(status_counts.items()):
+        if count != 0:
+            print("{}: {}".format(status_code, count))
+count = 0
+status_codes = Counter()
+size = 0
+try:
+    for line in sys.stdin:
+        if count != 0 and count % 10 == 0:
+            print_status(status_codes, size)
 
-    status_codes = Counter()
-    count = 0
-    size = 0
+        st_list = line.split()
 
-    try:
-        for line in sys.stdin:
-            if count != 0 and count % 10 == 0:
-                print_status(status_codes, size)
+        count += 1
 
-            st_list = line.split()
+        try:
+            size += int(st_list[-1])
+        except Exception:
+            pass
 
-            count += 1
-
-            try:
-                size += int(st_list[-1])
-            except Exception
-                pass
-
-            try:
-                status_code = st_list[-2]
-                status_codes[status_code] += 1
-            except
-                pass
-        print_status(status_codes, size)
-    except KeyboardInterrupt
-        print_status(status_codes, size)
+        try:
+            status_code = st_list[-2]
+            status_codes[status_code] += 1
+        except Exception:
+            pass
+    print_status(status_codes, size)
+except KeyboardInterrupt:
+    print_status(status_codes, size)
+    raise
