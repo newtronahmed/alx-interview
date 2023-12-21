78dbaa6fab75d827f0dd51f3190aeb4dcc5e0f78
neutronahmed <bazubairu@st.ug.edu.gh>
2023-12-21 23:41:19 +0000

    okay i rest my case

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,39 +1,47 @@
-#!/usr/bin/env python3
-"""Task 0 """
+#!/usr/bin/python3
 
+'''A script for parsing HTTP request logs.
+'''
 
 import sys
-from collections import Counter
 
-def print_status(status_counts, size):
-    """Prints status code and size"""
-    print("File size: {}".format(size))
-    for status_code, count in sorted(status_counts.items()):
-        if count != 0:
-            print("{}: {}".format(status_code, count))
+
+def printStatus(dic, size):
+    """ Prints information """
+    print("File size: {:d}".format(size))
+    for i in sorted(dic.keys()):
+        if dic[i] != 0:
+            print("{}: {:d}".format(i, dic[i]))
+
+
+# sourcery skip: use-contextlib-suppress
+statusCodes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+               "404": 0, "405": 0, "500": 0}
+
 count = 0
-status_codes = Counter()
 size = 0
+
 try:
     for line in sys.stdin:
         if count != 0 and count % 10 == 0:
-            print_status(status_codes, size)
+            printStatus(statusCodes, size)
 
-        st_list = line.split()
-
+        stlist = line.split()
         count += 1
 
         try:
-            size += int(st_list[-1])
+            size += int(stlist[-1])
         except Exception:
             pass
 
         try:
-            status_code = st_list[-2]
-            status_codes[status_code] += 1
+            if stlist[-2] in statusCodes:
+                statusCodes[stlist[-2]] += 1
         except Exception:
             pass
-    print_status(status_codes, size)
+    printStatus(statusCodes, size)
+
+
 except KeyboardInterrupt:
-    print_status(status_codes, size)
+    printStatus(statusCodes, size)
     raise
